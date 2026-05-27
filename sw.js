7522515e67583384de9668df75301b23490107e4
aexdm <adam@adam.dev>
2026-05-27 20:19:36 +0200

    restore sw.js (v2), manifest.json, _headers; add PWA link + sw register to index.html

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "adam-dev-v2";
+const FILES = [
+  "/",
+  "/index.html",
+  "/index.css",
+  "/index.js",
+  "/manifest.json",
+  "/404.html",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match("/")))
+  );
+});
